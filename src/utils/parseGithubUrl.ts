a3a35c0e7e4c82c4fa99ec8c8d053b079e026a7b
Jack W <jackweatherilt@outlook.com>
2024-03-20 21:57:44 +0000

    Error toast for incorrect Github URL parsing.

--- src/utils/parseGithubUrl.ts
+++ src/utils/parseGithubUrl.ts
@@ -1,8 +1,22 @@
+import { toast } from "sonner"
+
 export const parseGithubUrl = (githubUrl: string) => {
-  const url = new URL(githubUrl)
+  let url
+  try {
+    url = new URL(githubUrl)
+  } catch (error) {
+    toast.error("Oops, that doesn't look like a valid URL", {
+      position: "bottom-center",
+    })
+    return { match: false }
+  }
+
   // parse URL using regex groups e.g. https://github.com/octokit/rest.js/issues/405
-  const match = url.pathname.match(/\/([^/]+)\/([^/]+)\/issues\/(\d+)/)
+  const match = url.pathname.match(/\/([^/]+)\/([^/]+)\/issues\/(\d+)$/)
   if (!match) {
+    toast.error("Oops, that doesn't look like a GitHub URL", {
+      position: "bottom-center",
+    })
     return { match: false }
   }
   const [, org, repo, id] = match
