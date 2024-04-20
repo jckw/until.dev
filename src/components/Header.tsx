ae913df3aee184f7345f4d35317e5d7da7eb101c
Jack W <jackweatherilt@outlook.com>
2024-04-20 18:09:46 +0100

    UI component improvments.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,6 +31,50 @@ export const Header = ({
       {hideSearchBar ? null : (
         <div className="flex items-center justify-center relative">
           <Input
+            LeftIcon={() => (
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M12.6667 2H3.33333C2.59695 2 2 2.59695 2 3.33333V12.6667C2 13.403 2.59695 14 3.33333 14H12.6667C13.403 14 14 13.403 14 12.6667V3.33333C14 2.59695 13.403 2 12.6667 2Z"
+                  stroke="#A2AAB8"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M6 10L10 6"
+                  stroke="#A2AAB8"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            )}
+            RightIcon={() => (
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M5.99935 6.66675L2.66602 10.0001L5.99935 13.3334"
+                  stroke="#A2AAB8"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M13.3327 2.66675V7.33341C13.3327 8.04066 13.0517 8.71894 12.5516 9.21903C12.0515 9.71913 11.3733 10.0001 10.666 10.0001H2.66602"
+                  stroke="#A2AAB8"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            )}
             placeholder="Enter a Github Issue URL"
             spellCheck={false}
             type="url"
@@ -42,7 +86,7 @@ export const Header = ({
                 }
               }
             }}
-            className="max-w-96 text-center"
+            className="w-[200px] md:w-[400px] text-center"
             defaultValue={activeIssueUrl || ""}
           />
         </div>
--- src/ui/input.tsx
+++ src/ui/input.tsx
@@ -5,24 +5,40 @@ import { cn } from "@/utils"
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   variant?: "md" | "lg"
+  LeftIcon?: React.ComponentType<{ className?: string }>
+  RightIcon?: React.ComponentType<{ className?: string }>
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, variant = "md", ...props }, ref) => {
+  ({ className, type, variant = "md", LeftIcon, RightIcon, ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex w-full rounded-md border border-input bg-transparent shadow-sm transition-colors file:border-0 file:bg-transparent placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          variant === "md" &&
-            "h-9 px-3 py-1 text-sm file:text-sm file:font-medium",
-          variant === "lg" &&
-            "h-11 px-4 py-2 text-lg file:text-lg file:font-medium",
-          className
+      <div className="relative">
+        {LeftIcon && (
+          <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
+            <LeftIcon />
+          </div>
         )}
-        ref={ref}
-        {...props}
-      />
+        <input
+          type={type}
+          className={cn(
+            "flex w-full rounded-md border border-input bg-transparent transition-colors file:border-0 file:bg-transparent placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+            variant === "md" &&
+              "h-9 px-3 py-1 text-sm file:text-sm file:font-medium",
+            variant === "lg" &&
+              "h-11 px-4 py-2 text-lg file:text-lg file:font-medium",
+            LeftIcon && "pl-9",
+            RightIcon && "pr-9",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        {RightIcon && (
+          <div className="absolute right-3 top-1/2 transform -translate-y-1/2">
+            <RightIcon />
+          </div>
+        )}
+      </div>
     )
   }
 )
